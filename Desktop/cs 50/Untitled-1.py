15013a0ee7f7f5ae3fdcc80f072419aec42aba60
alikhantalal <alikhantalal7@gmail.com>
2024-05-02 02:00:00 +0500

    Description of changes

--- Desktop/cs 50/Untitled-1.py
+++ Desktop/cs 50/Untitled-1.py
@@ -1,12 +1,24 @@
-from flask import Flask,render_template
+from flask import Flask, render_template, session, url_for, redirect
+from flask_session import Session
+from tempfile import mkdtemp
 
 app = Flask(__name__)
+app.config["SESSION_FILE_DIR"] = mkdtemp()
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
 
 @app.route("/")
 def index():
-    return render_template("index.html")
-@app.route("/ali")
-def ali():
-    return "hello ali"
+    if "board" not in session:
+        session["board"] = [[None, None, None], [None, None, None], [None, None, None]]
+        session["turn"] = "x"
+        
+    return render_template("index.html", index=session["board"], turn=session["turn"])
+
+@app.route("/play/<int:i>/<int:j>")
+def play(i, j):
+    return f"Played a move at position ({i}, {j})"
+
 if __name__ == "__main__":
     app.run(debug=True, port=5001)
